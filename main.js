14398ebc1c403f23868c71ca72198358fc0c1c74
Sandeep Yadav <rkysandeepyadav@gmail.com>
2023-07-12 12:34:01 +0530

    adding Awards section

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-// // Instantiate the Bootstrap carousel
-// $('.multi-item-carousel').carousel({
-//     interval: false
-//   });
-  
-//   // for every slide in carousel, copy the next slide's item in the slide.
-//   // Do the same for the next, next item.
-//   $('.multi-item-carousel .item').each(function(){
-//     var next = $(this).next();
-//     if (!next.length) {
-//       next = $(this).siblings(':first');
-//     }
-//     next.children(':first-child').clone().appendTo($(this));
-    
-//     if (next.next().length>0) {
-//       next.next().children(':first-child').clone().appendTo($(this));
-//     } else {
-//         $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-//     }
-//   });
+let slides = document.querySelectorAll(".slide")
+let counter = 0;
+console.log(slides);
+slides.forEach(function (slide, index)
+ { 
+    slide.style.left = `${index * 100}%`
+ }
+)
+
+function slideImage(counter) {
+    slides.forEach(function (slide)
+     { 
+        slide.style.transform = `translateX(${counter * 100+100}%)`
+     }
+    )
+}
+function gonext() {
+    // console.log(slides);
+    counter++;
+    // alert();
+    slideImage(counter)
+}
+function goprev() {
+    counter--;
+    // alert();
+    slideImage(counter)
+}
